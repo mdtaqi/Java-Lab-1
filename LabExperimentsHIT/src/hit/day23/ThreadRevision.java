3dc0bd8b66ef1b434c8f5c599edd187f998ee976
TAQI <TAQI@DESKTOP-OVBMF8S>
2021-06-30 07:20:23 +0530

    First Commit

---
+++ LabExperimentsHIT/src/hit/day23/ThreadRevision.java
@@ -0,0 +1,28 @@
+package hit.day23;
+
+public class ThreadRevision {
+	public ThreadRevision() {
+		Thread t=new Thread(new ThreadJob());
+		t.start();
+	}
+	public static void main(String[] args) {
+		//How to capture the main thread
+		//All java programs run in main thread by default.
+		new ThreadRevision();
+		Thread t=Thread.currentThread();
+		System.out.println(t);
+		//lazy for loop
+		for(int i=0;i<5;i++) {
+			try {
+				Thread.sleep(1000);
+			}catch(Exception e) {}
+			System.out.println(i);
+		}
+	}
+}
+class ThreadJob implements Runnable{
+	@Override
+	public void run() {
+		System.out.println("child thread is executed....");
+	}
+}
