24cafb1968fd4e693976657e71b6d69afd1df407
TAQI <TAQI@DESKTOP-OVBMF8S>
2021-09-11 20:29:05 +0530

    demo

---
+++ LabExperimentsHIT/src/hitmod2/day3/Demo3.java
@@ -0,0 +1,8 @@
+package hitmod2.day3;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		System.out.println("Is it working now.............");
+	}
+
+}
---
+++ LabExperimentsHIT/src/taqi/finaltest/DemoTest.java
@@ -0,0 +1,12 @@
+package taqi.finaltest;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		System.out.println("Why the eclipse not working for the Folder?\n"
+				+ "Reason: When we include the complete class or some folder,\n"
+				+ "It must be a java project but the classwork is not a java project,\n"
+				+ "so it is showing error, But when we include only one LabexperimentsHIT its working..\n"
+				+ "because it is a java project.........................................");
+	}
+}
